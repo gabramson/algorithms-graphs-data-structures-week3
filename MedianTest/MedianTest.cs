198a71db23ac69b1d6fd918e340506581e9d86b4
Gary Abramson <gary.abramson@gmail.com>
2017-10-04 20:22:43 -0500

    initial

---
+++ MedianTest/MedianTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MedianTest
+{
+    [TestClass]
+    public class MedianTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
